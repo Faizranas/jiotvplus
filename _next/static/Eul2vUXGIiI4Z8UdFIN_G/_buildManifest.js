88a9fa906093222e418ea70c34a159abaa357ce6
Anand Raut <anand.raut@ril.com>
2025-12-11 13:13:36 +0530

    Build v1.0.0

--- _next/static/Eul2vUXGIiI4Z8UdFIN_G/_buildManifest.js
+++ _next/static/Eul2vUXGIiI4Z8UdFIN_G/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/jiotvplus//_next/:path+",
+        "destination": "/jiotvplus/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
